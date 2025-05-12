af242366635a78246dab000b994efb1006a20838
IliasShadow <145995986+IliasShadow@users.noreply.github.com>
2025-05-12 13:23:42 +1000

    Add files via upload

---
+++ JavaScript/filter.js
@@ -0,0 +1,57 @@
+﻿let correspond = {
+    "Название": "structure",
+    "Тип": "category",
+    "Страна": "country",
+    "Город": "city",
+    "Год": ["yearFrom", "yearTo"],
+    "Высота": ["heightFrom", "heightTo"]
+}
+let dataFilter = (dataForm) => {
+    let dictFilter = {};
+    for(let j = 0; j < dataForm.elements.length; j++) {
+        let item = dataForm.elements[j];
+        let valInput = item.value;
+        if (item.type == "text") {
+            valInput = valInput.toLowerCase();
+        } else if (item.type == "number") {
+            if (valInput !== "") {
+                valInput = Number(valInput);
+            } else {
+                if (item.id.includes("From")) {
+                    valInput = -Infinity;
+                }
+                else if (item.id.includes("To")) {
+                    valInput = Infinity;
+                }
+            }
+        }
+        dictFilter[item.id] = valInput;
+    }       
+    return dictFilter;
+}
+let filterTable = (data, idTable, dataForm) =>{
+    let datafilter = dataFilter(dataForm);
+    let tableFilter = data.filter(item => {
+        let result = true;
+        for(let key in item) {
+            let val = item[key];
+            if (typeof val == 'string') {
+                val = item[key].toLowerCase() 
+                result &&= val.indexOf(datafilter[correspond[key]]) !== -1 
+            } else if (typeof val == 'number') {
+                let range = correspond[key]; 
+                let from = datafilter[range[0]]; 
+                let to = datafilter[range[1]];
+                result &&= val >= from && val <= to;
+            }
+         }
+         return result;
+    });     
+    clearTable(idTable);
+    createTable(tableFilter, idTable);  
+}
+let clearFilter = (idTable, data, dataForm) => {
+    dataForm.reset();
+    clearTable(idTable);
+    createTable(data, idTable);
+};
---
+++ JavaScript/main.js
@@ -0,0 +1,51 @@
+﻿let createOption = (str, val) => {
+    let item = document.createElement('option');
+    item.text = str;
+    item.value = val;
+    return item;
+}
+let setSortSelect = (arr, sortSelect) => {
+    sortSelect.append(createOption('Нет', 0));
+    for (let i in arr) {
+        sortSelect.append(createOption(arr[i], Number(i) + 1));
+    }
+}
+let setSortSelects = (data, dataForm) => { 
+    let head = Object.keys(data[0]);
+    let allSelect = dataForm.getElementsByTagName('select');
+    for(let j = 0; j < allSelect.length; j++) {
+        setSortSelect(head, allSelect[j]);
+        if (j > 0) {
+            allSelect[j].disabled = true;
+        }
+    }
+}
+let changeNextSelect = (nextSelectId, curSelect) => {
+    let nextSelect = document.getElementById(nextSelectId);
+    nextSelect.disabled = false;
+    nextSelect.innerHTML = curSelect.innerHTML;
+    if (curSelect.value != 0) {
+       nextSelect.remove(curSelect.value);
+    } else {
+        nextSelect.disabled = true;
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    createTable(buildings, 'list');
+    let clearFilt = document.getElementById('clearFilter');
+    document.getElementById('find').onclick = function() {
+        filterTable(buildings, "list", document.getElementById("filter"))
+    }
+    document.getElementById('clearFilter').onclick = function(){
+        clearFilter("list", buildings, document.getElementById("filter"));
+        clearSortTable(document.getElementById('sort'));
+    }
+    setSortSelects(buildings,document.getElementById("sort"));
+    document.getElementById("fieldsFirst").onchange = function() {
+        changeNextSelect('fieldsSecond', document.getElementById('fieldsFirst'));
+    };
+    document.getElementById("sortTable").onclick = function() {
+        sortTable('list', document.getElementById('sort'));
+    };
+});
+
